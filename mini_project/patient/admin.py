351a8755671c0f8aa06cf5e47bd64fdf744b36cd
Vyshnavi-V <vvyshnavi2020@gmail.com>
2023-06-11 20:59:04 +0530

    Updated

--- mini_project/patient/admin.py
+++ mini_project/patient/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PatientDetails,Booking,Otp
+from .models import PatientDetails,Booking,Otp,HealthRecord
 
 admin.site.register(PatientDetails)
 admin.site.register(Booking)
 admin.site.register(Otp)
+admin.site.register(HealthRecord)
